5c9cf9f4a91b0eb8aeab765da8de8cd512a6d9cf
Petr Valenta <petr.valenta.92@gmail.com>
2020-07-03 17:16:56 +0200

    Creating of main() function Adding of icons

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+
+# Intialize the pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800, 600)) # width(x) and hight(y), pixels
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("alien.png")
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 500
+playerX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x,y))
+
+# Game Loop
+running = True
+while running:
+
+    # RGB - Red, Green, Blue
+    screen.fill((79, 91, 102))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+        # if keystroke is pressed, check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update()
